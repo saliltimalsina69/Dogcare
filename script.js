4f0745bdb7cb1a4de8a8df8b7cb2c2a79fdf0f79
saliltimalisna <salil.ragani@gmail.com>
2021-07-18 20:15:20 +0545

    ok

---
+++ script.js
@@ -0,0 +1,9 @@
+// const hamburger = document.querySelector('.hamburger-btn');
+// const nav = document.querySelector('.nav-bar');
+
+// hamburger.addEventListener("click", ()=>{
+//     nav.classList.toggle("open");
+//     hamburger.classList.toggle("rotate");
+// })
+
+
